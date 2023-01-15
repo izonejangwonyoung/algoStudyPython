8bb8a1008b1fe56c19fdc7cb092b88e2956c49ba
izonejangwonyoung <2000shim@naver.com>
2023-01-15 19:02:05 +0900

    20230115

---
+++ firstTry/test.py
@@ -0,0 +1,18 @@
+def star(n):
+    if n == 3:
+        return ['***', '* *', '***']
+
+    stars = star(n // 3)
+    print(stars)
+    arr = []
+    for s in stars:
+        arr.append(s * 3)
+    for s in stars:
+        arr.append(s + ' ' * (n // 3) + s)
+    for s in stars:
+        arr.append(s * 3)
+    return arr
+
+
+n = int(input())
+print(*star(n), sep='\n')
